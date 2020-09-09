aa06ec5adb4788c163f62e0dfd3894007819d4cd
lukiko <cooper.barryw@gmail.com>
2020-09-09 17:01:30 -0400

    test

--- CMSC495_Group3/src/RentalExpress.java
+++ CMSC495_Group3/src/RentalExpress.java
@@ -284,7 +284,7 @@ public class RentalExpress {
     hellcatBtn.setVerticalTextPosition(SwingConstants.BOTTOM);
     hellcatBtn.setHorizontalTextPosition(SwingConstants.CENTER);
     hellcatBtn.setFocusPainted(false);
-    carPanel.add(hellcatBtn);
+    //carPanel.add(hellcatBtn);
     
     ImageIcon camry = new ImageIcon("src/resources/camry.jpg");
     JToggleButton camryBtn = new JToggleButton("<html><center>2020 Toyota Camry TRD<br> $96/day </center></html>", camry);
@@ -302,7 +302,7 @@ public class RentalExpress {
       public void actionPerformed(ActionEvent e) {
       }
     });
-    carPanel.add(camryBtn);
+    //carPanel.add(camryBtn);
     
     ImageIcon corvette = new ImageIcon("src/resources/corvette.jpg");
     JToggleButton corvetteBtn = new JToggleButton("<html><center>2020 Chevrolet Corvette<br> $135/day </center></html>", corvette);
@@ -313,7 +313,7 @@ public class RentalExpress {
     corvetteBtn.setVerticalTextPosition(SwingConstants.BOTTOM);
     corvetteBtn.setHorizontalTextPosition(SwingConstants.CENTER);
     corvetteBtn.setFocusPainted(false);
-    carPanel.add(corvetteBtn);
+    //carPanel.add(corvetteBtn);
     
     
     
@@ -398,7 +398,7 @@ public class RentalExpress {
     
     carBtn.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent arg0) {
-        card.show(cardPanel,  "p5");
+        card.show(cardPanel,  "p3");
       }
     });
     
